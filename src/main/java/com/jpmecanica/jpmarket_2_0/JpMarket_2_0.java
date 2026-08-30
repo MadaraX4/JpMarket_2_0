b64d7e3c0fb95b8f878df57427522b62368e194a
MadaraX4 <lordtrevas1998@hotmail.com>
2026-08-30 10:33:02 -0300

    primeiro commit

---
+++ src/main/java/com/jpmecanica/jpmarket_2_0/JpMarket_2_0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jpmecanica.jpmarket_2_0;
+
+/**
+ *
+ * @author lordt
+ */
+public class JpMarket_2_0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
